daefd47b6973f8b229245e0a023920d3a5ef2c32
sunilpshenoy <sunilpshenoy@gmail.com>
2025-07-15 17:59:36 +0000

    auto-commit for 43a0ccd4-b02f-4079-a27b-ee480b30c47c

--- final_backend_test.py
+++ final_backend_test.py
@@ -44,12 +44,13 @@ class ComprehensiveBackendTester:
         timestamp = int(time.time())
         username = f"testuser_{timestamp}{username_suffix}"
         email = f"{username}@test.com"
-        password = "testpass123"
+        # Use environment variable for test password or generate secure one
+        test_password = os.environ.get('TEST_PASSWORD', 'TestPass123!@#$')
         
         user_data = {
             "username": username,
             "email": email,
-            "password": password,
+            "password": test_password,
             "display_name": f"Test User {username_suffix}"
         }
         
@@ -63,7 +64,7 @@ class ComprehensiveBackendTester:
                 self.users[username] = {
                     "username": username,
                     "email": email,
-                    "password": password,
+                    "password": test_password,
                     "token": token,
                     "user_id": user_id,
                     "display_name": user_data["display_name"]
