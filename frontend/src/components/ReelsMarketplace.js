4687f8147728639747b430370f89b6a15be7003e
sunilpshenoy <sunilpshenoy@gmail.com>
2025-07-15 18:46:33 +0000

    auto-commit for b75b9c9d-7f86-46fb-a763-e01a6394a0f5

--- frontend/src/components/ReelsMarketplace.js
+++ frontend/src/components/ReelsMarketplace.js
@@ -470,7 +470,10 @@ const ReelsMarketplace = ({ user, token, api }) => {
           </button>
 
           {/* Chat Button */}
-          <button className="flex flex-col items-center space-y-1 text-white">
+          <button 
+            className="flex flex-col items-center space-y-1 text-white"
+            aria-label={`Start a chat with ${reel.seller.name} about ${reel.service.title}`}
+          >
             <div className="bg-black bg-opacity-30 p-3 rounded-full">
               <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
                 <path fillRule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clipRule="evenodd"/>
