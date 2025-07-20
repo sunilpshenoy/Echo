ed076da522d60916c7dc7bb9a0985685923e1d8d
sunilpshenoy <sunilpshenoy@gmail.com>
2025-07-20 06:13:16 +0000

    auto-commit for ae918219-6e59-4f42-acd7-c81188d3049e

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import axios from 'axios';
 import './App.css';
 import AuthScreen from './components/AuthScreen';
