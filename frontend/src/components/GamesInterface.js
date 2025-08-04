c1e4b01e15e543936d5cda2c825d6f02223085c3
sunilpshenoy <sunilpshenoy@gmail.com>
2025-08-04 18:16:23 +0000

    auto-commit for c9c46288-e3fb-428c-a3d9-f9312d337c47

--- frontend/src/components/GamesInterface.js
+++ frontend/src/components/GamesInterface.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import io from 'socket.io-client';
-import TicTacToeGame from './games/TicTacToe';
+import TicTacToeGame from './games/TicTacToeGame';
 import WordGuessGame from './games/WordGuessGame';
 import LudoGame from './games/LudoGame';
 import MafiaGame from './games/MafiaGame';
