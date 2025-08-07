b1915a33046239812139b5c4f4fb8b51f37fa51c
sunilpshenoy <sunilpshenoy@gmail.com>
2025-08-07 06:21:20 +0000

    auto-commit for 996e316f-b3d4-480a-ad58-207bc86a436e

--- auth_websocket_test.py
+++ auth_websocket_test.py
@@ -373,7 +373,7 @@ class AuthWebSocketTester:
             
             start_time = time.time()
             websocket = await asyncio.wait_for(
-                websockets.connect(websocket_url, timeout=10),
+                websockets.connect(websocket_url),
                 timeout=15.0
             )
             response_time = time.time() - start_time
